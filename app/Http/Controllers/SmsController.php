27283ff12739ed634ad15421ae6913de0e11a631
johnnybirds05 <johnmichaelcagadas@gmail.com>
2024-10-05 16:50:25 +0800

    update msg

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Customer;
 use App\Models\Delivery;
 use App\Models\Vehicle;
@@ -10,56 +11,58 @@ use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
-    public function smsCustomer($id){
-        $fetch_data = Delivery::where('delivery_id',$id)->first();
-        $vehicle = Vehicle::where('vehicle_id',$fetch_data->vehicle_id)->first();
-        $customer = Customer::where('customer_id',$fetch_data->customer_id)->first();
-        $driver = Driver::where('driver_id',$fetch_data->driver_id)->first();
+    public function smsCustomer($id)
+    {
+        $fetch_data = Delivery::where('delivery_id', $id)->first();
+        $vehicle = Vehicle::where('vehicle_id', $fetch_data->vehicle_id)->first();
+        $customer = Customer::where('customer_id', $fetch_data->customer_id)->first();
+        $driver = Driver::where('driver_id', $fetch_data->driver_id)->first();
         $ch = curl_init();
         $apiKey =  env('SMS');
         $parameters = array(
             'apikey' => $apiKey,
             'number' => $customer->contact_no,
-            'message' => 'Hello Mr/Mrs. '.$customer->lname.'! A '.$vehicle->name.' is coming to pickup your package. Driver: '.$driver->lname.' '.$driver->contact_no,
+            'message' => 'Hello Mr/Mrs. ' . $customer->lname . '! A ' . $vehicle->name . ' is coming to pickup your package. Driver: ' . $driver->lname . ' ' . $driver->contact_no,
             'sendername' => 'LARATSYS'
         );
-        curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/priority' );
-        curl_setopt( $ch, CURLOPT_POST, 1 );
+        curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
+        curl_setopt($ch, CURLOPT_POST, 1);
 
         //Send the parameters set above with the request
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
 
         // Receive response from server
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        $output = curl_exec( $ch );
-        curl_close ($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
 
         return $output;
     }
-    
-    public function smsReciever($id){
-        $fetch_data = Delivery::where('delivery_id',$id)->first();
-        $vehicle = Vehicle::where('vehicle_id',$fetch_data->vehicle_id)->first();
-        $customer = Customer::where('customer_id',$fetch_data->customer_id)->first();
-        $driver = Driver::where('driver_id',$fetch_data->driver_id)->first();
+
+    public function smsReciever($id)
+    {
+        $fetch_data = Delivery::where('delivery_id', $id)->first();
+        $vehicle = Vehicle::where('vehicle_id', $fetch_data->vehicle_id)->first();
+        $customer = Customer::where('customer_id', $fetch_data->customer_id)->first();
+        $driver = Driver::where('driver_id', $fetch_data->driver_id)->first();
         $ch = curl_init();
         $apiKey =  env('SMS');
         $parameters = array(
             'apikey' => $apiKey,
             'number' => $fetch_data->reciever_no,
-            'message' => 'Hello Mr/Mrs. '.$fetch_data->reciever_name.'! A '.$vehicle->name.' is coming to deliver a package from '.'Mr/Mrs. '.$customer->lname.'. Driver: '.$driver->lname.' '.$driver->contact_no,
+            'message' => 'Hello Mr/Mrs. ' . $fetch_data->reciever_name . '! A ' . $vehicle->name . ' is coming to deliver a package from ' . 'Mr/Mrs. ' . $customer->lname . '. Driver: ' . $driver->lname . ' ' . $driver->contact_no,
             'sendername' => 'LARATSYS'
         );
-        curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/priority' );
-        curl_setopt( $ch, CURLOPT_POST, 1 );
+        curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
+        curl_setopt($ch, CURLOPT_POST, 1);
 
         //Send the parameters set above with the request
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
 
         // Receive response from server
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        $output = curl_exec( $ch );
-        curl_close ($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
 
         return $output;
     }
